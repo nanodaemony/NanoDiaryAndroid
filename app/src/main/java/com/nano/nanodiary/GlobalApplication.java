9afa1be38ca04ba59ea9b5759a35eb2bb5422e44
chen zeng <chenzeng@yuanfudao.com>
2021-08-08 20:54:17 +0800

    First init.

---
+++ app/src/main/java/com/nano/nanodiary/GlobalApplication.java
@@ -0,0 +1,72 @@
+package com.nano.nanodiary;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import java.io.IOException;
+import java.util.Properties;
+import lombok.Getter;
+
+
+/**
+ * 应用Application类
+ *
+ * @author nano
+ */
+public class GlobalApplication extends Application {
+
+//    /**
+//     * 日志
+//     */
+//    @Getter
+//    private static Logger logger = new Logger("main");
+
+    /**
+     * 实现SharedPreference,用于实现关键信息的本地存储
+     */
+    private static SharedPreferences shareMap;
+    private static SharedPreferences.Editor shareEditor;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        basicInit();
+        // 静态类初始化 初始化顺序有要求!
+//        LogStatic.init();
+//        TaskExecutor.init();
+//        PersistUtil.init();
+//        TimeStampUtils.init();
+//        AppStatic.init();
+    }
+
+//    /**
+//     * 初始化工作
+//     */
+//    private void basicInit() {
+//        shareMap = getSharedPreferences("CommonData", MODE_PRIVATE);
+//        shareEditor = shareMap.edit();
+//        // 读取配置文件
+//        AppStatic.properties = new Properties();
+//        try {
+//            AppStatic.properties.load(getApplicationContext().getAssets().open("config.properties"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        boolean debug = Boolean.parseBoolean(AppStatic.properties.getProperty("Debug"));
+//        //logger.info(debug + "");s
+//    }
+//
+//    /**
+//     * Getter and setters
+//     *
+//     * @return SharedPreferences
+//     */
+//    public static SharedPreferences getShareMap() {
+//        return shareMap;
+//    }
+//
+//    public static SharedPreferences.Editor getShareEditor() {
+//        return shareEditor;
+//    }
+
+}
---
+++ app/src/main/java/com/nano/nanodiary/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.nano.nanodiary;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Description: 主页Activity
+ *
+ * @version: 1.0
+ * @author: nano
+ * @date: 2021/8/7 11:27 下午
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home);
+
+
+
+    }
+
+}
